1aa3d043a176e10ae43910feb7279fec0e55b158
Facus006 <facu.calabrese06@gmail.com>
2023-09-06 20:26:08 -0300

    Inicio del proyecto

---
+++ src/main/java/com/equipoC/Trendytouch/TrendytouchApplication.java
@@ -0,0 +1,13 @@
+package com.equipoC.Trendytouch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendytouchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendytouchApplication.class, args);
+	}
+
+}
