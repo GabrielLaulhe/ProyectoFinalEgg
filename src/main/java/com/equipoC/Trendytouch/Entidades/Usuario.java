5bd8bec5e01b2b7069a4c2077069d11f30bbf451
Facus006 <facu.calabrese06@gmail.com>
2023-09-07 17:10:27 -0300

    creacion de la entidad Usuario sin los atributos de imagen, rol y reporte

---
+++ src/main/java/com/equipoC/Trendytouch/Entidades/Usuario.java
@@ -0,0 +1,36 @@
+package com.equipoC.Trendytouch.Entidades;
+
+import java.util.Date;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Usuario {
+    
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+    
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String nombreusuario;
+    private String password;
+    private String pregunta;
+    private String respuesta;
+    private boolean alta;
+    private boolean diseñador;
+    private Date fecharegistro;
+    private Date fechabaja;
+    private Date ultimavez;
+    
+    
+}
