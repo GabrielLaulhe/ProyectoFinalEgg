8fba6197163c61026a60a4d7e76711876beb9899
Facus006 <facu.calabrese06@gmail.com>
2023-09-07 21:05:53 -0300

    Arreglo de varios errores en clase usuario y una query para buscar por nombreUsuario

--- src/main/java/com/equipoC/Trendytouch/Entidades/Usuario.java
+++ src/main/java/com/equipoC/Trendytouch/Entidades/Usuario.java
@@ -1,38 +1,48 @@
 package com.equipoC.Trendytouch.Entidades;
 
 import java.util.Date;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 
-
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Usuario {
-    
+
     @Id
     @GeneratedValue(generator = "uuid")
     @GenericGenerator(name = "uuid", strategy = "uuid2")
     private String id;
-    
+
     private String nombre;
     private String apellido;
     private String email;
-    private String nombreusuario;
+    private String nombreUsuario;
     private String password;
     private String pregunta;
     private String respuesta;
     private boolean alta;
     private boolean diseñador;
-    private Date fecharegistro;
-    private Date fechabaja;
-    private Date ultimavez;
-    private Imagen imagen;
     
+    @Temporal(TemporalType.DATE)
+    private Date fechaRegistro;
+    @Temporal(TemporalType.DATE)
+    private Date fechaBaja;
+    @Temporal(TemporalType.DATE)
+    private Date ultimaVez;
+
+    @OneToOne
+    private Imagen imagen;
+
 }
