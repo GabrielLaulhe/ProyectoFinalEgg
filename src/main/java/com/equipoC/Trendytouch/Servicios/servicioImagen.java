eff11f7fa8f8d829e4626b28fa641d33ac024cdf
Facus006 <facu.calabrese06@gmail.com>
2023-09-08 19:47:42 -0300

    movi el imagen servicio a Servicios

--- src/main/java/com/equipoC/Trendytouch/Servicios/servicioImagen.java
+++ src/main/java/com/equipoC/Trendytouch/Servicios/servicioImagen.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.equipoC.Trendytouch.servicio;
+package com.equipoC.Trendytouch.Servicios;
 
 import com.equipoC.Trendytouch.Entidades.Imagen;
 import com.equipoC.Trendytouch.excepciones.MiException;
