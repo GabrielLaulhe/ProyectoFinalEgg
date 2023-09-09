567d45c232741319d7daa1d5f391736bd38fcb6a
Gabriel Laulhe <gabriellole@hotmail.com>
2023-09-08 22:08:54 -0300

    carga seguridadWeb, se crea el servicio comentario y una modificacion de usuarioServicio

---
+++ src/main/java/com/equipoC/Trendytouch/SeguridadWeb.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.equipoC.Trendytouch;
+
+import com.equipoC.Trendytouch.Servicios.UsuarioServicio;
+import static javafx.scene.input.KeyCode.T;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**/
+/**
+ *
+ * @author Asus
+ */
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SeguridadWeb extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public UsuarioServicio usuarioServicio;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder aut) throws Exception {
+        aut.userDetailsService(usuarioServicio)
+                .passwordEncoder(new BCryptPasswordEncoder());
+               
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .antMatchers("/admin/*").hasRole("ADMIN")
+                .antMatchers("/css/*", "/js/*", "/img/*", "/**")
+                .permitAll()
+                .and().formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/logincheck")//debe coincidir con el action del html
+                .usernameParameter("email")
+                .passwordParameter("password")
+                .defaultSuccessUrl("/inicio") //pagina luego del login
+                .permitAll()
+                .and().logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")//debe retornar al index
+                .permitAll()
+                .and().csrf()
+                .disable();
+    }
+
+}
