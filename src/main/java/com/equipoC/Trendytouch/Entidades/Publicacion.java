1f0f242dd0effeb30cc8516018440789b69e3ce4
“gonzalolucien” <gonzalolucien@gmail.com>
2023-09-08 21:08:29 -0300

    ARREGLO DE PROPIEDADES Y ATRIBUTOS DE CLASE USUARIO

--- src/main/java/com/equipoC/Trendytouch/Entidades/Publicacion.java
+++ src/main/java/com/equipoC/Trendytouch/Entidades/Publicacion.java
@@ -4,40 +4,50 @@ package com.equipoC.Trendytouch.Entidades;
 import java.util.Date;
 import java.util.List;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 
 @Entity
-@NoArgsConstructor @AllArgsConstructor
-@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Publicacion {
     @Id
     @GeneratedValue(generator = "uuid")
     @GenericGenerator(name = "uuid", strategy = "uuid2")
     private String id;
-    private String categoria;
+    
     private String descripcion;
    
-    @OneToOne
-    private Reporte reportes;
+    @OneToMany
+    private List<Reporte> reportes;
+    
+    @ManyToOne
+    private Usuario usuario;
+    
+    @OneToMany
+    private List<Usuario> megusta;
+    
+    @OneToMany
+    private List<Imagen> fotos;
+    
     @OneToMany
     private List<Comentario> comentarios;
-    @OneToMany
-    private List<Imagen> imagenes;
     
-    /* No se si sera necesario estos otros atributos
+    @Enumerated(EnumType.STRING)
+    private Categoria categoria;
+    
     @Temporal(TemporalType.DATE)
     private Date fechaPublicacion;
-    @Temporal(TemporalType.DATE)
-    private Date fechaRetiro;   
-    */
+    
 }
