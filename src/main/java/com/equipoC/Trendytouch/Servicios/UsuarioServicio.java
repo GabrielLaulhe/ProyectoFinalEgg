208df7cc8332dac38f9c6d92f3b9dcc285af88b2
Facus006 <facu.calabrese06@gmail.com>
2023-09-14 19:31:48 -0300

    eliminar error servicio y eliminar usuario

--- src/main/java/com/equipoC/Trendytouch/Servicios/UsuarioServicio.java
+++ src/main/java/com/equipoC/Trendytouch/Servicios/UsuarioServicio.java
@@ -150,6 +150,18 @@ public class UsuarioServicio implements UserDetailsService {
     }
 
     @Transactional
+    public void eliminar(String id) throws MyException {
+
+        try {
+            Usuario usuario = usuariorepo.getById(id);
+            usuariorepo.delete(usuario);
+        } catch (Exception e) {
+            throw new MyException(e.getMessage());
+        }
+
+    }
+
+    @Transactional
     public void cambiarRol(String id, String rol) {
         Optional<Usuario> respuesta = usuariorepo.findById(id);
 
