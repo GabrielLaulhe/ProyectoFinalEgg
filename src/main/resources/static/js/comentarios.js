444969b1452f3fadb946ddc813314f710cf44154
Miguel Ortegon <miguelangelortegon10@gmail.com>
2023-09-25 17:13:20 -0500

    Añado estilo a tarjetas y comentarios

---
+++ src/main/resources/static/js/comentarios.js
@@ -0,0 +1,8 @@
+function mostrarVentana() {
+    document.getElementById('ventana-difuminada').style.display = 'flex';
+}
+
+function ocultarVentana() {
+    document.getElementById('ventana-difuminada').style.display = 'none';
+}
+
