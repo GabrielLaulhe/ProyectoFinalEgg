4892855611f465d6c60751d74027033592f73f29
“gonzalolucien” <gonzalolucien@gmail.com>
2023-09-26 23:06:38 -0300

    Arreglo de comentario

--- src/main/resources/static/js/comentarios.js
+++ src/main/resources/static/js/comentarios.js
@@ -1,16 +1,24 @@
-function mostrarVentana() {
-    document.getElementById('ventana-difuminada').style.display = 'flex';
+function mostrarVentana(id) {
+    const publicacion = document.getElementById(id);
+    const ventanaDifuminada = publicacion.querySelector('#ventana-difuminada');
+    ventanaDifuminada.style.display = 'flex';
 }
 
-function ocultarVentana() {
-    document.getElementById('ventana-difuminada').style.display = 'none';
+function ocultarVentana(id) {
+    const publicacion = document.getElementById(id);
+    const ventanaDifuminada = publicacion.querySelector('#ventana-difuminada');
+    ventanaDifuminada.style.display = 'none';
 }
 
-function mostrarVentana2() {
-    document.getElementById('ventana-difuminada2').style.display = 'flex';
+function mostrarVentana2(id) {
+    const publicacion = document.getElementById(id);
+    const ventanaDifuminada2 = publicacion.querySelector('#ventana-difuminada2');
+    ventanaDifuminada2.style.display = 'flex';
 }
 
-function ocultarVentana2() {
-    document.getElementById('ventana-difuminada2').style.display = 'none';
+function ocultarVentana2(id) {
+    const publicacion = document.getElementById(id);
+    const ventanaDifuminada2 = publicacion.querySelector('#ventana-difuminada2');
+    ventanaDifuminada2.style.display = 'none';
 }
 
