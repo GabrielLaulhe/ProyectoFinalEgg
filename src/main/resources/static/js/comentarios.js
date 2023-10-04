999ac9a311ae1eed8179e4160dcb75368e92e52e
Miguel Ortegon <miguelangelortegon10@gmail.com>
2023-10-03 19:31:33 -0500

    Hago cambios finales en tarjeta

--- src/main/resources/static/js/comentarios.js
+++ src/main/resources/static/js/comentarios.js
@@ -22,3 +22,14 @@ function ocultarVentana2(id) {
     ventanaDifuminada2.style.display = 'none';
 }
 
+function mostrarVentana3(id) {
+    const publicacion = document.getElementById(id);
+    const ventanaDifuminada2 = publicacion.querySelector('#ventana-difuminada3');
+    ventanaDifuminada2.style.display = 'flex';
+}
+
+function ocultarVentana3(id) {
+    const publicacion = document.getElementById(id);
+    const ventanaDifuminada2 = publicacion.querySelector('#ventana-difuminada3');
+    ventanaDifuminada2.style.display = 'none';
+}
